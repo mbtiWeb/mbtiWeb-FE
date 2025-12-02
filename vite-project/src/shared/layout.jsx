b10410e3cb21a2db429100971165e800ca4b34da
yun1215 <145582431+seoyunl1215@users.noreply.github.com>
2025-12-02 16:10:37 +0900

    css수정

--- vite-project/src/shared/layout.jsx
+++ vite-project/src/shared/layout.jsx
@@ -4,7 +4,10 @@ import Header from '../components/Header.jsx';
 
 const layoutStyles = {
     minHeight: '80vh', // Header와 Footer를 제외한 본문 영역 높이
-    padding: '20px',
+    padding: '4px',
+    paddingTop: '60px',
+    paddingRight: '10px',
+    paddingLeft: '10px',
 };
 
 function Layout({ children }) {
